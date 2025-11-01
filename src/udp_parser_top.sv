5b860c1603f123e4f8f04ee6c81387ae17fa503e
Toby <tobywr64@gmail.com>
2025-11-01 16:50:22 +0000

    Added packet_start logic and other improvements.

--- src/udp_parser_top.sv
+++ src/udp_parser_top.sv
@@ -5,6 +5,7 @@ module udp_parser_top (
     input logic [15:0] target_port,
 
     input logic [7:0] data_in,
+    input logic packet_start,
     input logic data_valid_in,
     output logic ready_out,
 
@@ -16,39 +17,33 @@ module udp_parser_top (
     output logic [15:0] src_port,
     output logic [15:0] dst_port,
     output logic [15:0] length,
-    output logic header_done,
-
-    output logic [15:0] latched_cycle_count
+    output logic header_done
 );
 
   //defining internal wires
-  logic parse_enable, latch_outputs, fwd_enable, drop_enable;
+  logic parse_enable, fwd_enable, drop_enable;
   logic [15:0] udp_length_wire, checksum_wire;
-  logic port_match_wire;
-  logic last_in_wire;
-  logic [15:0] global_byte_count;
-  logic checksum_ok = 1'b1;  //for the time being, checksum is disabled.
-  logic is_first_byte;
-  assign is_first_byte = counter_rst && data_valid_in;
-  logic header_done_wire;
-  assign length = udp_length_wire;
-  assign header_done = header_done_wire;
+  logic port_match_wire, checksum_ok_wire;
+  logic packet_last_wire;
+  logic [15:0] raw_checksum;
+  //skip checksum as not implemented correctly yet.
+  assign checksum_ok_wire = 1'b1;
 
   //INSTANTATE ALL LOGIC
   control_FSM u_control_FSM (
       .clk(clk),
       .rst_n(rst_n),
       .data_valid_in(data_valid_in),
-      .last_in(payload_last),
-      .header_done(header_done_wire),
+      .packet_last(packet_last_wire),
+      .header_done(header_done),
       .port_match(port_match_wire),
-      .checksum_ok(checksum_ok),
+      .checksum_ok(checksum_ok_wire),
       .parse_enable(parse_enable),
-      .latch_outputs(latch_outputs),
       .fwd_enable(fwd_enable),
       .drop_enable(drop_enable),
       .counter_rst(counter_rst),
-      .counter_enable(counter_enable)
+      .counter_enable(counter_enable),
+      .packet_start(packet_start)
   );
 
   payload_forwarder u_payload_forwarder (
@@ -64,7 +59,7 @@ module udp_parser_top (
       .payload_valid_out(payload_valid_out),
       .payload_last(payload_last),
       .ready_in(ready_in),
-      .header_done(header_done_wire)
+      .header_done(header_done)
   );
 
   udp_header_parser u_udp_header_parser (
@@ -75,22 +70,22 @@ module udp_parser_top (
       .ready_out(ready_out),
       .target_port(target_port),
       .parse_enable(parse_enable),
-      .latch_outputs(latch_outputs),
       .src_port(src_port),
       .dst_port(dst_port),
       .port_match(port_match_wire),
       .length(udp_length_wire),
-      .checksum(checksum_wire),
-      .header_done(header_done_wire)
+      .checksum(raw_checksum),
+      .header_done(header_done)
   );
-
-  cycle_counter u_cycle_counter (
+  
+  packet_byte_counter u_packet_byte_counter (
       .clk(clk),
       .rst_n(rst_n),
-      .start_counter(is_first_byte),
-      .stop_counter(payload_last),
-      .cycle_count(latched_cycle_count),
-      .is_counting()
+      .counter_rst(counter_rst),
+      .counter_enable(counter_enable),
+      .udp_length(udp_length_wire),
+      .packet_last(packet_last_wire)
   );
 
+  assign length = udp_length_wire;
 endmodule
